3786dfdbe33799d4853dd82eb153664077b1b224
RiteshJadhav021 <riteshjadhav3435@gmail.com>
2025-09-12 03:03:31 +0530

    connect to database

---
+++ Backend/src/db.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',         // replace with your MySQL username
+  password: '8432513435Ritesh', // replace with your MySQL password
+  database: 'store_rating_app'
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
+    return;
+  }
+  console.log('Connected to MySQL database!');
+});
+
+module.exports = connection;
